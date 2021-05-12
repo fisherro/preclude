6b02174f0223591f5f23268b118452d2af7b4ad2
fisherro <fisherro@users.noreply.github.com>
2021-05-12 15:24:17 -0500

    First checking of source file!

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-CXXFLAGS+=-std=c++20
+#CXXFLAGS+=-std=c++20
+CXXFLAGS+=-std=c++2a
 CXXFLAGS+=-fsanitize=address
 #CXXFLAGS+=-fcoroutines
 #CXXFLAGS+=-pthread
@@ -9,8 +10,9 @@ CXXFLAGS+=-Werror=vla
 CXXFLAGS+=-pedantic
 CXXFLAGS+=-pedantic-errors
 
-LDFLAGS+=-lm
-LDFLAGS+=-latomic
-LDFLAGS+=-lstdc++fs
+#LDFLAGS+=-lm
+#LDFLAGS+=-latomic
+#LDFLAGS+=-lstdc++fs # GCC requires prior to 9.1
+#LDFLAGS+=-lc++fs # LLVM requires prior to 9.0
 
 all: preclude
---
+++ preclude.cpp
@@ -0,0 +1,77 @@
+//TODO: Need a list of file extensions to parse.
+//TODO: Need a list of files/extensions to not copy.
+#include <cstdlib>
+#include <exception>
+#include <filesystem>
+#include <iostream>
+#include <unordered_set>
+
+namespace fs = std::filesystem;
+
+//No cross-platform support for hidden files in <filesystem>. Sigh.
+bool is_hidden(const fs::path& p)
+{
+    //TODO: Use u8string?
+    std::string fn{p.filename()};
+    //TODO: Use starts_with
+    if (fn.empty()) return true; //??
+    return '.' == fn[0];
+}
+
+void parse_it(const fs::path& p)
+{
+    std::cout << "Parsing " << p << '\n';
+}
+
+void copy_it(const fs::path& p)
+{
+    std::cout << "Copying " << p << '\n';
+}
+
+int main(const int argc, const char** argv)
+{
+    try {
+        const std::unordered_set<std::string> to_parse{".html"};
+        const std::unordered_set<std::string> dont_copy{".htmlf"};
+
+        if (argc < 3) {
+            std::cerr << "Usage: " << argv[0]
+                << " input_directory output_directory\n";
+            return EXIT_FAILURE;
+        }
+
+        const fs::path indir{argv[1]};
+        const fs::path outdir{argv[2]};
+
+        //To prevent recursion into hidden directories,
+        //we need access to the iterator.
+        //So, we can't use a ranged-for or other higher-level iteration.
+        for (fs::recursive_directory_iterator i{indir};
+                i != fs::recursive_directory_iterator{};
+                ++i)
+        {
+            if (is_hidden(i->path())) {
+                if (i->is_directory()) {
+                    i.disable_recursion_pending();
+                }
+                continue;
+            }
+            //TODO: Use contains
+            if (to_parse.count(i->path().extension()) > 0) {
+                parse_it(i->path());
+            } else if (dont_copy.count(i->path().extension()) < 1) {
+                copy_it(i->path());
+            } else {
+                std::cout << "Skipping " << i->path() << '\n';
+            }
+        }
+
+        return EXIT_SUCCESS;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << '\n';
+        return EXIT_FAILURE;
+    } catch (...) {
+        std::cerr << "Unrecognized exception\n";
+        return EXIT_FAILURE;
+    }
+}
